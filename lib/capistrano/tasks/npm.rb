e31eb28a682345a16f36f99bf754a1af3ca138b9
William DA SILVA <william.da.silva@outlook.com>
2019-12-29 18:28:14 +0100

    fix: update capistrano tasks & npm start script

--- lib/capistrano/tasks/npm.rb
+++ lib/capistrano/tasks/npm.rb
@@ -4,7 +4,7 @@ namespace :npm do
     on roles(:all) do
       within release_path do
         execute 'docker-compose', "-f #{fetch :docker_file}", :run, 'back', 'npm ci'
-        execute 'docker-compose', "-f #{fetch :docker_file}", :run, 'app', 'npm ci && npm run build'
+        execute 'docker-compose', "-f #{fetch :docker_file}", :run, 'app', 'bash -c "npm ci && npm run build"'
       end
     end
   end
