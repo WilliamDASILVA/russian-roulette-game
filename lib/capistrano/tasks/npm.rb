143ad820afef49f6f011b4418f76f43293c1d63d
William DA SILVA <william.da.silva@outlook.com>
2019-12-29 18:15:20 +0100

    build: update production tasks

--- lib/capistrano/tasks/npm.rb
+++ lib/capistrano/tasks/npm.rb
@@ -4,16 +4,7 @@ namespace :npm do
     on roles(:all) do
       within release_path do
         execute 'docker-compose', "-f #{fetch :docker_file}", :run, 'back', 'npm ci'
-        execute 'docker-compose', "-f #{fetch :docker_file}", :run, 'app', 'npm ci'
-      end
-    end
-  end
-
-  desc 'Build project'
-  task :build do
-    on roles(:all) do
-      within release_path do
-        execute 'docker-compose', "-f #{fetch :docker_file}", :run, 'app', 'npm run build'
+        execute 'docker-compose', "-f #{fetch :docker_file}", :run, 'app', 'npm ci && npm run build'
       end
     end
   end
